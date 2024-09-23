ab5ad0164fd9bffa4f3d785f64d4b511a905b7b5
utkarshg1 <gaikwadujg@gmail.com>
2024-09-23 14:05:56 +0530

    basic html done

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -18,19 +18,35 @@ monthly_challenges = {
 }
 
 
+def get_months():
+    return list(monthly_challenges.keys())
+
+
+def index(request):
+    list_items = ""
+    months = get_months()
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href={month_path}>{capitalized_month}</li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     try:
-        months = list(monthly_challenges.keys())
+        months = get_months()
         redirect_month = months[month-1]
         redirect_path = reverse("month-challenge", args=[redirect_month])
         return HttpResponseRedirect(redirect_path)
     except:
-        return HttpResponseNotFound("Please enter valid month between 1 to 12")
+        return HttpResponseNotFound("<h1>Please enter valid month between 1 to 12</h1>")
 
 
 def monthly_challenge(request, month):
     challenge_text = monthly_challenges.get(month)
     if challenge_text is not None:
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     else:
-        return HttpResponseNotFound("Please enter valid month!")
+        return HttpResponseNotFound("<h1>Please enter valid month!</h1>")
